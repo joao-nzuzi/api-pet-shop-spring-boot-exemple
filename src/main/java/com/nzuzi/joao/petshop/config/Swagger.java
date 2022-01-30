0d0708713666f3b40f21452a81edf3d05599230d
joao-nzuzi <joaonzuzi21@gmail.com>
2022-01-30 23:12:47 +0100

    Configuração do Swagger para documentação da API

---
+++ src/main/java/com/nzuzi/joao/petshop/config/Swagger.java
@@ -0,0 +1,41 @@
+package com.nzuzi.joao.petshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("PetShop Api")
+                .description("Exemplo de uma API Spring Boot para PetShop")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("João Nzuzi", "@jn", "@nzuzi"))
+                .build();
+    }
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.nzuzi.joao.petshop.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
